80804180835914052a9f09d1d7cd89d471188317
Bartek <bartekma89@gmail.com>
2017-12-18 21:06:19 +0100

    ready to push

--- js/script.js
+++ js/script.js
@@ -1,11 +1,38 @@
-function sayHello() {
-	console.log(this);
+function Button(text) {
+	this.text = text || 'Hello!';
+	this.color;
+	this.width;
+	this.height;
 }
 
-var person = {
-	name: 'Jan', 
-	sayHello: function() {
-		console.log('Hello ' + name);
+Button.prototype = {
+	create: function() {
+		var self = this;
+		this.color = prompt("Which color of button do You want?", 'blue');
+		this.width = prompt("Write width", '75px');
+		this.height = prompt("Write height", '50px');
+		this.text = prompt('Create you text', this.text);
+		this.$element = $('<button>');
+		this.$element.text(this.text);
+		this.$element.css({
+			'color': '#fff',
+			'margin': '5px',
+			'border-radius': '5px',
+			'backgroundColor': this.color,
+			'border': 'none',
+			'width': this.width,
+			'height': this.height,
+			'cursor': 'pointer'
+		});
+		this.$element.click(function() {
+			alert(self.text);
+			var btn = new Button();
+			btn.create();
+		});
+		
+		$('body').append(this.$element);
 	}
 };
 
+var btn = new Button();
+btn.create();
